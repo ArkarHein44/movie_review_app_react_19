6f705c630c4a0d071c4ed33ef4b847a5222a32d1
arkarhein <arkerhein44@gmail.com>
2026-01-08 21:53:28 +0630

    API test OK

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search.jsx';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
 const API_OPTIONS = {
     method: 'GET',
     headers:{
@@ -21,16 +22,13 @@ const App = ()=>{
         try{
             const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const response = await fetch(endpoint, API_OPTIONS);
-            
-            console.log(response);
 
              if(!response.ok){
                 throw new Error(`Failed to fetch movies`)
              }
 
-             const data = await response.json();
-             console.log(data);
-
+            const data = await response.json();
+            console.log(data);
         }catch(error){
             console.log(`Error fetching movies: ${error}`);
             setErrorMessage('Failed to fetch movies. Please try again later.');
@@ -53,7 +51,7 @@ const App = ()=>{
                 </header>
 
                 <section className="all-movies">
-                    <p className="error-message text-red">{errorMessage}</p>
+                    <p className="error-message text-red-500">{errorMessage}</p>
                 </section>
                 
                 
