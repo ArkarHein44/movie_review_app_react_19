a9354875e971adde3a3b1b968ebcf451b1f398b1
arkarhein <arkerhein44@gmail.com>
2026-01-09 10:02:16 +0630

    lt ch

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = ()=>{
             <div className="pattern" />
             <div className="wrapper">
                 <header>
-                        <img src="./hero.png" alt="hero banner" />
+                    <img src="./hero.png" alt="hero banner" />
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                 </header>
 
